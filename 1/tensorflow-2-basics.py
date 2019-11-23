4f5f46267fe8948f03ce24d93b3803935bf8fd25
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-23 07:24:11 -0500

    Finish up the basics

--- 1/tensorflow-2-basics.py
+++ 1/tensorflow-2-basics.py
@@ -23,3 +23,17 @@ print('sqrt =', np.sqrt(tensor), '\n')
 
 # Dot product
 print('[[1,2],[3,4]]\u00B7[[23,4],[32,51]] =' , np.dot(tensor, tensor_20))
+
+# Operations with variables
+tf2_variable = tf.Variable([[1., 2., 3.], [4., 5., 6.]])
+print(tf2_variable, '\n')
+print('variable.numpy = {}'.format(tf2_variable.numpy()), '\n')
+
+# Strings
+tf_string = tf.constant('Tensorflow string')
+print('tf string length = ', tf.strings.length(tf_string), '\n')
+print('tf string unicode decode = ', tf.strings.unicode_decode(tf_string, 'UTF8'), '\n')
+tf_string_array = ['an', 'array', 'of', 'strings']
+for i, s in enumerate(tf_string_array):
+    print('{} in the array is = {}\n'.format(i, s))
+
