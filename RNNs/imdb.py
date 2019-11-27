c2a80d5fd11b9e85100628b9c4ad83927b26a307
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-27 08:14:39 -0500

    Add, compile, eval model

--- RNNs/imdb.py
+++ RNNs/imdb.py
@@ -19,3 +19,26 @@ X_test = tf.keras.preprocessing.sequence.pad_sequences(
 # Embed layer parameters
 vocab_size = number_of_words
 embed_size = 128
+
+model = tf.keras.models.Sequential()
+
+# Adding the embeding layer
+model.add(tf.keras.layers.Embedding(
+    vocab_size, embed_size, input_shape=(X_train.shape[1],)))
+
+# Add the LSTM Layer
+# 128 units, tanh activation
+model.add(tf.keras.layers.LSTM(units=128, activation='tanh'))
+
+# Adding the dense output layer
+model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Compile, fit, evaluate
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=3, batch_size=128)
+
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+model.summary()
+print("Test accuracy: {}\nTest loss: {}".format(test_accuracy, test_loss))
