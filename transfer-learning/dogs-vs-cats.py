7ff54a7196985e88822cf6146ced1a9fe66ea676
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-28 05:24:25 -0500

    Setup, train, eval model

--- transfer-learning/dogs-vs-cats.py
+++ transfer-learning/dogs-vs-cats.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm_notebook
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-dataset_path = "./transfer-learning/cats_and_dogs_filtered.zip"
+dataset_path = "./cats_and_dogs_filtered.zip"
 zip_object = zipfile.ZipFile(file=dataset_path, mode="r")
 zip_object.extractall("./")
 zip_object.close()
@@ -33,3 +33,22 @@ prediction_layer = tf.keras.layers.Dense(
 model = tf.keras.models.Model(
     inputs=base_model.input, outputs=prediction_layer)
 # model.summary()
+
+# Compile
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001),
+              loss="binary_crossentropy", metrics=["accuracy"])
+
+# Create data generators
+data_gen_train = ImageDataGenerator(rescale=1/255.)
+data_gen_valid = ImageDataGenerator(rescale=1/255.)
+train_generator = data_gen_train.flow_from_directory(
+    train_dir, target_size=(128, 128), batch_size=128, class_mode="binary")
+valid_generator = data_gen_valid.flow_from_directory(
+    validation_dir, target_size=(128, 128), batch_size=128, class_mode="binary")
+
+# Train model
+model.fit_generator(train_generator, epochs=5, validation_data=valid_generator)
+
+# Evaluate
+valid_loss, valid_accuracy = model.evaluate_generator(valid_generator)
+print("Accuracy after transfer learning: {}".format(valid_accuracy))
