5e360ee0fe79310000b6f18570d767f4e7ede035
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-28 05:06:04 -0500

    Load pre-trained model, freeze, define head

--- transfer-learning/dogs-vs-cats.py
+++ transfer-learning/dogs-vs-cats.py
@@ -13,3 +13,23 @@ zip_object.close()
 dataset_path_new = "./cats_and_dogs_filtered/"
 train_dir = os.path.join(dataset_path_new, "train")
 validation_dir = os.path.join(dataset_path_new, "validation")
+
+# Loading the pre-trained model
+IMG_SHAPE = (128, 128, 3)
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=IMG_SHAPE, include_top=False, weights="imagenet")
+
+# base_model.summary()
+
+# Freeze the base model
+base_model.trainable = False
+
+# Define a custom head
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+prediction_layer = tf.keras.layers.Dense(
+    units=1, activation='sigmoid')(global_average_layer)
+
+# Define the model
+model = tf.keras.models.Model(
+    inputs=base_model.input, outputs=prediction_layer)
+# model.summary()
