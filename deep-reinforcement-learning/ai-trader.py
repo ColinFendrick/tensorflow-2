dae6db4778e273b31b481e53c4165597f24baeb1
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-30 05:32:12 -0500

    Create helper functions

--- deep-reinforcement-learning/ai-trader.py
+++ deep-reinforcement-learning/ai-trader.py
@@ -62,6 +62,39 @@ class AI_Trader():
         if self.epsilon > self.epsilon_final:
             self.epsilon *= self.epsilon_decay
 
-something = AI_Trader(10)
-print(something.model.summary())
 
+def sigmoid(x):
+    return 1/(1+math.exp(-x))
+
+
+def stocks_price_format(n):
+    if n < 0:
+        return "- $ {0:2f}".format(abs(n))
+    else:
+        return "$ {0:2f}".format(abs(n))
+
+
+def dataset_loader(stock_name):
+    # Complete the dataset loader function
+    dataset = data_reader.DataReader(stock_name, data_source="yahoo")
+
+    start_date = str(dataset.index[0]).split()[0]
+    end_date = str(dataset.index[-1]).split()[0]
+    close = dataset['Close']
+
+    return close
+
+
+def state_creator(data, timestep, window_size):
+    starting_id = timestep - window_size + 1
+
+    if starting_id >= 0:
+        windowed_data = data[starting_id:timestep+1]
+    else:
+        windowed_data = - starting_id * [data[0]] + list(data[0:timestep+1])
+
+    state = []
+    for i in range(window_size - 1):
+        state.append(sigmoid(windowed_data[i+1] - windowed_data[i]))
+
+    return np.array([state])
