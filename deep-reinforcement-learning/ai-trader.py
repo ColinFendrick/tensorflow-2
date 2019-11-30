06303b12ea26948c1918fa90b127784a719748c1
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-30 05:59:20 -0500

    Add the train loop

--- deep-reinforcement-learning/ai-trader.py
+++ deep-reinforcement-learning/ai-trader.py
@@ -91,13 +91,14 @@ def state_creator(data, timestep, window_size):
     if starting_id >= 0:
         windowed_data = data[starting_id:timestep+1]
     else:
-        windowed_data = - starting_id * [data[0]] + list(data[0:timestep+1])
+        windowed_data = -starting_id * [data[0]] + list(data[0:timestep+1])
 
     state = []
     for i in range(window_size - 1):
         state.append(sigmoid(windowed_data[i+1] - windowed_data[i]))
 
     return np.array([state])
+
 
 # Load the dataset
 stock_name = "AAPL"
@@ -112,3 +113,49 @@ data_samples = len(data) - 1
 
 # Define trader model
 trader = AI_Trader(window_size)
+
+# Training loop
+for episode in range(1, episodes + 1):
+    print("Episode: {}/{}".format(episode, episodes))
+
+    state = state_creator(data, 0, window_size + 1)
+
+    total_profit = 0
+    trader.inventory = []
+
+    for t in tqdm(range(data_samples)):
+        action = trader.trade(state)
+        next_state = state_creator(data, t+1, window_size + 1)
+        reward = 0
+
+        if action == 1:  # Buying
+            trader.inventory.append(data[t])
+            print("AI Trader bought: ", stocks_price_format(data[t]))
+
+        elif action == 2 and len(trader.inventory) > 0:  # Selling
+            buy_price = trader.inventory.pop(0)
+
+            reward = max(data[t] - buy_price, 0)
+            total_profit += data[t] - buy_price
+            print("AI Trader sold: ", stocks_price_format(
+                data[t]), " Profit: " + stocks_price_format(data[t] - buy_price))
+            
+        if t == data_samples - 1:
+            done = True
+        else:
+            done = False
+        
+        trader.memory.append((state, action, reward, next_state, done))
+
+        state = next_state
+
+        if done:
+            print("########################")
+            print("TOTAL PROFIT: {}".format(total_profit))
+            print("########################")
+        
+        if len(trader.memory) > batch_size:
+            trader.batch_train(batch_size)
+        
+    if episode % 10 == 0:
+        trader.model.save("ai_trader_{}.h5".format(episode))
