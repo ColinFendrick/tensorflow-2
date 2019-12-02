be87a7eefd8ba28c01fe866f5b60486983be26f2
Colin Fendrick <colin.fendrick@gmail.com>
2019-12-02 08:06:12 -0500

    Compile, fit, evaluate server

--- tensorflow-serving/tensorflow-serving.py
+++ tensorflow-serving/tensorflow-serving.py
@@ -32,3 +32,14 @@ model.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(units=128, activation='relu'))
 model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+
+# Compile
+model.compile(optimizer='Adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['sparse_categorical_accuracy'])
+
+model.fit(X_train, y_train, batch_size=128, epochs=10)
+
+# Evaluate
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Test accuracy is {}".format(test_accuracy))
