387f00c7a37b117c903d91a94af81e90fcc7e595
Colin Fendrick <colin.fendrick@gmail.com>
2019-12-02 08:04:56 -0500

    Create model for serving

---
+++ tensorflow-serving/tensorflow-serving.py
@@ -0,0 +1,34 @@
+import os
+import json
+import random
+import requests
+import subprocess
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import cifar10
+
+# Load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+class_names = ['airplane', 'automobile', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Normalize images
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Define model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3,
+                                 padding="same", activation="relu", input_shape=[32, 32, 3]))
+model.add(tf.keras.layers.Conv2D(
+    filters=32, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+model.add(tf.keras.layers.Conv2D(
+    filters=64, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.Conv2D(
+    filters=64, kernel_size=3, padding="same", activation="relu"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
